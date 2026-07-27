e3096f926b01e33d11169f545b0b86c6c86e5d32
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-07-27 23:44:48 +0500

    Add hello world java program

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {//In Java, a public class name must match the file name exactly.
+    public static void main(String[] args) {// shortcut for this line is psvm. write it and press enter to get this syntax.
+        System.out.print("Hello, World!");
+    }
+    
+}
