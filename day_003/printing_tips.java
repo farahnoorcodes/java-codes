c37b3db652d73e0fecc0c0858195b7f86a94a6e1
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-07-29 21:55:05 +0500

    java codes update

---
+++ day_003/printing_tips.java
@@ -0,0 +1,13 @@
+package day_003;// it just show the folder in which we are working
+
+public class printing_tips {
+    public static void main(String[] args) {//psvm+Enter is used to get this line , it's a shortcut.
+        System.out.println("helloworld!");//println is used to add a new line after writting the text. sout+Enter is a shortcut to add it automatically.
+        System.out.println("hello farah!!");
+        System.out.println(8);// output: 8 it is without qoutes "".
+        System.out.println(8+7);//output: 15 , it can do mathematical operation own it's own.
+        System.out.println("6+7");//output: 6+7 , because it is in double qoutes, it will be treated as string(word, letter, sentence)
+        System.out.println("8");// output: 8, but you can do any operation with it , cuz its a string now
+    }
+    
+}
